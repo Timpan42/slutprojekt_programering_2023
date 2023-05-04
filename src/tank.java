b4f2832f0b21baa1f64bb26733c20ba186f924cc
Timpan42 <77719322+Timpan42@users.noreply.github.com>
2023-05-04 12:31:44 +0200

    tank har nu all information den behöver.

--- src/tank.java
+++ src/tank.java
@@ -1,3 +1,55 @@
 public class tank {
     public int hp = 3;
+    public int warPoints;
+    public int x;
+    public int y;
+    public int damage = 1;
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getWarPoints() {
+        return warPoints;
+    }
+
+    public void setWarPoints(int warPoints) {
+        this.warPoints = warPoints;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getDamage(){
+        return damage;
+    }
+
+    public void shoot(){
+        warPoints -= 2;
+    }
+    public void move(){
+        warPoints -= 1;
+    }
+    public void heal(){
+        warPoints -= 2;
+        hp += 1;
+    }
 }
+
